49bcb6f1b44c613fbcc38c2fea6b98dc5016cd28
SERGIO MARTIN RUBIO <elsergiobcn@gmail.com>
2021-11-19 21:41:40 +0100

    Corregim redirecció dashboard

--- www/teacher_planner/src/Security/LoginFormAuthenticator.php
+++ www/teacher_planner/src/Security/LoginFormAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse('dashboard');
+        return new RedirectResponse('/courses');
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
