1307eeda7db7a57a3f55285777ace7a9fe273b82
SERGIO MARTIN RUBIO <elsergiobcn@gmail.com>
2021-11-20 16:54:19 +0100

    Restringim possibles usuaris duplicats

--- www/teacher_planner/src/Entity/User.php
+++ www/teacher_planner/src/Entity/User.php
@@ -2,13 +2,18 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="L'usuari ja existeix"
+ * )
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
